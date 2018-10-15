090ec0dcd40d59be8113daad27492cdb65490042
leigh blechman <leigh.blechman@gmail.com>
2018-10-15 00:32:45 -0400

    plz

---
+++ build/precache-manifest.a7e1cb7ac7f34d4395fc87538102045d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2eea535106e466a026e5f3003b6b4a06",
+    "url": "/static/media/mariostartlarge.2eea5351.jpg"
+  },
+  {
+    "revision": "5bda3c1b8e503b3993624c7586947a93",
+    "url": "/static/media/mariostart.5bda3c1b.jpg"
+  },
+  {
+    "revision": "6c02f15fdbc9dd7c482b52b06d8e0a6c",
+    "url": "/static/media/SuperMario256.6c02f15f.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a8b8723042363f5fbe5b",
+    "url": "/static/js/main.a8b87230.chunk.js"
+  },
+  {
+    "revision": "bfc04fa9f4e31d5d836b",
+    "url": "/static/js/1.bfc04fa9.chunk.js"
+  },
+  {
+    "revision": "a8b8723042363f5fbe5b",
+    "url": "/static/css/main.2ea4a7ea.chunk.css"
+  },
+  {
+    "revision": "81d42eb42cd258cea9a3cbc922673612",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4cfc7c445e52e5300e5e997682b26b10.js"
+  "/precache-manifest.a7e1cb7ac7f34d4395fc87538102045d.js"
 );
 
 workbox.clientsClaim();
